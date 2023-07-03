b675856069110f3097d4f4f01115f1c32e5ce7cf
nikiforov <nikiforov@selectel.ru>
2023-07-03 16:08:30 +0300

    disabled dropbar

--- lua/core/plugins/dropbear.lua
+++ lua/core/plugins/dropbear.lua
@@ -3,5 +3,6 @@ local utils = require("core.utils.functions")
 return {
   "Bekaboo/dropbar.nvim",
   event = { "BufReadPre", "BufNewFile" },
-  enabled = utils.isNeovimVersionsatisfied(10),
+  -- enabled = utils.isNeovimVersionsatisfied(10),
+  enabled = false,
 }
